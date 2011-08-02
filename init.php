45cec16e8cd21463f506ca45602eccfcfb2ae92f
stopkin <stopkin0@gmail.com>
2011-08-02 13:54:23 +0700

    checks if class page exists or not

--- init.php
+++ init.php
@@ -1,6 +1,13 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-$langs = Page::instance()->system_langs();
+$langs = NULL;
+if(class_exists('Page'))
+{
+    /**
+     * Load language conf
+     */
+    $langs = Page::instance()->system_langs();
+}
 
 Route::set('comments', '(<lang>/)comment(/<action>(/<type>)(/<object_id>(/<place>)(/<visibility>)))', array(
 	'lang'       => $langs,
