9f2b8d453574d0dcffb26811f8c517f0ccb117e5
smgladkovskiy <SMGladkovskiy@gmail.com>
2011-08-15 09:50:02 +0400

    some fixes and refactoring

--- views/frontend/content/comments/tree.php
+++ views/frontend/content/comments/tree.php
@@ -2,11 +2,11 @@
 <h3><a name="comments"><?php echo __('Comments')?>:</a></h3>
 <?php if(isset($_user)):?>
 <?php StaticJs::instance()->add("
-	var comment_link = '".Route::url('comments', array('action' => 'add', 'type' => $comment_type, 'lang' => i18n::lang()))."';
+	var comment_link = '".$comment_link."';
 	var comment_button = '".__('to_comment')."';
 ", NULL, 'inline')?>
 <div id="comment_main">
-<?php echo Form::open(Route::url('comments', array('action' => 'add', 'type' => $comment_type, 'object_id' => $last_comment_id, 'place' => $place, 'lang' => i18n::lang())))?>
+<?php echo Form::open($main_comment_link)?>
 	<div class="form-item"><?php echo Form::textarea('text')?></div>
 	<div class="form-item"><?php echo Form::button(NULL, __('to_comment'))?></div>
 <?php echo Form::close()?>
